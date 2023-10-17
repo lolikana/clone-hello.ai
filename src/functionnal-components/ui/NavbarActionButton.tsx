bba0621af749521ba0beaa17accc5d1f85f6eec6
Lolikana <thomas.guiart@gmail.com>
2023-10-17 11:48:02 +0900

    add navbar actions and animation

---
+++ src/functionnal-components/ui/NavbarActionButton.tsx
@@ -0,0 +1,50 @@
+import { type FC, useState } from 'react';
+
+import styles from './NavbarActionButton.module.scss';
+
+type Props = {
+	label: 'Motion' | 'Language';
+	button1: 'Auto' | 'Ja';
+	button2: 'Pause' | 'En';
+};
+
+const NavbarActionButton: FC<Props> = ({ label, button1, button2 }) => {
+	const [isPressed, setIsPressed] = useState<boolean>(true);
+
+	const toggleIsPressed = () => {
+		setIsPressed(prevState => !prevState);
+	};
+
+	const content = (
+		<div className={styles.navbar__action}>
+			<p className={styles.navbar__action_label} role="menuitem">
+				{label}
+			</p>
+			<p className={styles.navbar__action_buttons}>
+				<button
+					onClick={() => toggleIsPressed()}
+					role="menuitem"
+					aria-label={`${button1}`}
+					aria-pressed={isPressed}
+					disabled={isPressed}
+				>
+					{button1}
+				</button>
+				<span> /&nbsp;</span>
+				<button
+					onClick={() => toggleIsPressed()}
+					role="menuitem"
+					aria-label={`${button2}`}
+					aria-pressed={!isPressed}
+					disabled={!isPressed}
+				>
+					{button2}
+				</button>
+			</p>
+		</div>
+	);
+
+	return content;
+};
+
+export default NavbarActionButton;
